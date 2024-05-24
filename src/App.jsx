4a762abc9effb6942cdade65e75334ce64531c9e
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-05-24 16:22:24 +0530

    inputBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import InputBox from "./components/InputBox"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat" style={{backgroundImage :`url(https://store-images.s-microsoft.com/image/apps.3790.13746602931236419.ec145616-d3b2-4fef-b955-55708f912cfd.44b1adcc-6220-448a-ada7-ec31717c5e62?mode=scale&q=90&h=1080&w=1920)`}}>
+      {/* <h1>Hello </h1> */}
+      <InputBox />
+    </div>
   )
 }
