09e517b7d6fc7b919dff58f148c2894edf15a616
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-07-01 19:59:55 +0530

    hooks folder added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
 import InputBox from "./components/InputBox"
 
 export default function App() {
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState('usd')
+  const [to, setTo] = useState('inr')
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+
   return (
     <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat" style={{backgroundImage :`url(https://store-images.s-microsoft.com/image/apps.3790.13746602931236419.ec145616-d3b2-4fef-b955-55708f912cfd.44b1adcc-6220-448a-ada7-ec31717c5e62?mode=scale&q=90&h=1080&w=1920)`}}>
-      {/* <h1>Hello </h1> */}
       <InputBox />
     </div>
   )
