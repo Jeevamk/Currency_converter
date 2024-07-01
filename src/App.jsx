f2ad08f49800b914763584af7aa1ab9805383def
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-07-01 20:38:33 +0530

    hooks folder added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
 import { useState } from "react"
-import InputBox from "./components/InputBox"
+import {InputBox} from "./components/index.js"
+import useCurrencyInfo from "./hooks/useCurrencyInfo"
 
 export default function App() {
   const [amount, setAmount] = useState(0)
   const [from, setFrom] = useState('usd')
   const [to, setTo] = useState('inr')
-  const [convertedAmount, setConvertedAmount] = useState(0)
+  const [convertedAmount, setConvertedAmount] = useState()
+
+  //custom hooks
+ const currencyInfo = useCurrencyInfo(from)
+//  const options = Object.keys(currencyInfo)
+const options = currencyInfo ? Object.keys(currencyInfo) : [];
+
+ const swap =() =>{
+  setFrom(to)
+  setTo(from)
+  setConvertedAmount(amount)
+  setAmount(convertedAmount)
+ }
+
+ const convert =() =>{
+  setConvertedAmount(amount * currencyInfo[to])
+ }
 
 
   return (
     <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat" style={{backgroundImage :`url(https://store-images.s-microsoft.com/image/apps.3790.13746602931236419.ec145616-d3b2-4fef-b955-55708f912cfd.44b1adcc-6220-448a-ada7-ec31717c5e62?mode=scale&q=90&h=1080&w=1920)`}}>
-      <InputBox />
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+         <form onSubmit={(e)=>{e.preventDefault()
+          convert()
+         }}>
+          <div className="w-full mb-1"> 
+            <InputBox  label="from" amount={amount} 
+            currencyOptions={options} 
+            onCurrencyChange={(currency)=> setFrom(currency)} 
+            onAmountChange={(amount)=>setAmount(amount)} 
+            selectedCurrency={from} />
+
+          </div>
+         </form>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React,{useId} from 'react'
 
 const InputBox = ({label,amount,onAmountChange,onCurrencyChange,currencyOptions=[],selectedCurrency = "usd",amountDisabled=false,currencyDisable =false,className="",}) => {
+  const id = useId()
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className='w-1-2'>
-        <label className='text-black/40 mb-2 inline-block'>{label}</label>
-        <input type="number" className='outline-none w-full bg-transparent py-1.5' placeholder='Amount' disabled={amountDisabled} value={amount} onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))} />
+        <label htmlFor={id} className='text-black/40 mb-2 inline-block'>{label}</label>
+        <input id={id} type="number" className='outline-none w-full bg-transparent py-1.5' placeholder='Amount' disabled={amountDisabled} value={amount} onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))} />
       </div>
       <div className='w-1/2 flex flex-wrap justify-end text-right'>
         <p className='text-black/40 mb-2 w-full'>Currency Type</p>
